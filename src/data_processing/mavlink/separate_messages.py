f41997a1e9c017d5b62294917f187cde559aaed3
Josh Meise <joshua.m.meise.24@dartmouth.edu>
2025-10-31 18:20:20 +0000

    binary support added to message separation

--- src/data_processing/mavlink/separate_messages.py
+++ src/data_processing/mavlink/separate_messages.py
@@ -2,80 +2,115 @@
 #
 # Josh Meise
 # 10-31-2025
-# Description: Input file should be full binary MAVLink dataset.
-#              -h flag indicates that headers should be written to file too
+# Description: -h flag indicates that headers should be written to file too
 #              absence of -h flag indicates only payloads should be written
+#              -b flag indicates that input file is a binary file
 #
 
 import sys
 import os
 
 def main():
-    if len(sys.argv) != 3 and len(sys.argv) != 4:
-        print("usage: python3 separate_messages.py <infile.bin> <outdir> [-h]")
+    if len(sys.argv) not in (3, 4, 5):
+        print("usage: python3 separate_messages.py <infile.bin> <outdir> [-h] [-b]")
         sys.exit(1)
 
-    if len(sys.argv) == 4 and sys.argv[3] != "-h":
-        print("usage: python3 separate_messages.py <infile.bin> <outdir> [-h]")
+    if len(sys.argv) == 4 and sys.argv[3] not in ("-h", "-b"):
+        print("usage: python3 separate_messages.py <infile.bin> <outdir> [-h] [-b]")
         sys.exit(1)
-    
+
+    if len(sys.argv) == 5:
+        flags = sys.argv[3:]
+        if not(("-h" in flags) and ("-b" in flags)):
+            print("usage: python3 separate_messages.py <infile.bin> <outdir> [-h] [-b]")
+            sys.exit(1)
+
     infile = sys.argv[1]
     outdir = sys.argv[2]
-    if len(sys.argv) == 4: headers = True
-    else: headers = False
+    
+    headers = False
+    binary = False
+    
+    if len(sys.argv) > 3:
+        headers = "-h" in sys.argv[3:]
+        binary = "-b" in sys.argv[3:]
 
     file_size = os.path.getsize(infile)
 
     ids_written = set()
 
-    with open(infile, "rb") as ifile:
+    if binary: 
+        read_type = "rb"
+        mult = 1
+    else: 
+        read_type = "r"
+        mult = 2
+
+    with open(infile, read_type) as ifile:
         bytes_processed = 0
         
         while bytes_processed < file_size:
-            code = ifile.read(1)
-            length = ifile.read(1)
+            code = ifile.read(1*mult)
+            length = ifile.read(1*mult)
             string = code + length
 
-            # MAVLink 2 read in flags.
-            if code == b'\xfd':
-                inc_flags = ifile.read(1)
-                cmp_flags = ifile.read(1)
+            if not binary: 
+                byte_code = bytes.fromhex(code)
+                byte_length = bytes.fromhex(length)
+            else: 
+                byte_code = code
+                byte_length = length
+
+            # MAVLink 2 has flags.
+            if byte_code == b'\xfd':
+                inc_flags = ifile.read(1*mult)
+                cmp_flags = ifile.read(1*mult)
                 string += (inc_flags + cmp_flags)
-            
-            seq = ifile.read(1)
-            sys_id = ifile.read(1)
-            comp_id = ifile.read(1)
+                
+                if not binary: byte_inc_flags = bytes.fromhex(inc_flags)
+                else: byte_inc_flags = inc_flags
 
-            #MAVLink 2 ahs 3-byte message ID, MAVLink 1 has 1 byte
-            if code == b'\xfd':
-                msg_id = ifile.read(3)
-            elif code == b'\xfe':
-                msg_id = ifile.read(1)
+            seq = ifile.read(1*mult)
+            sys_id = ifile.read(1*mult)
+            comp_id = ifile.read(1*mult)
 
-            payload = ifile.read(int.from_bytes(length))
-            checksum = ifile.read(2)
+            # MAVLink 2 has 3-byte message ID, MAVLink 1 has 1 byte
+            if byte_code == b'\xfd': msg_id = ifile.read(3*mult)
+            elif byte_code == b'\xfe': msg_id = ifile.read(1*mult)
+
+            payload = ifile.read(int.from_bytes(byte_length)*mult)
+            checksum = ifile.read(2*mult)
             string += (seq + sys_id + comp_id + msg_id + payload + checksum)
 
             # If signed message.
-            if code == b'\xfd' and inc_flags == b'\x01':
-                signature = ifile.read(13)
+            if byte_code == b'\xfd' and byte_inc_flags == b'\x01':
+                signature = ifile.read(13*mult)
                 string += signature
-
+            
             bytes_processed += len(string)
 
-            msg_id_int = int.from_bytes(msg_id, byteorder = "little")
+            # Read newline character if ASCII.
+            if not binary:
+                ifile.read(1)
+                bytes_processed += 1
+
+            if binary: msg_id_int = int.from_bytes(msg_id, byteorder = "little")
+            else: msg_id_int = int.from_bytes(bytes.fromhex(msg_id), byteorder = "little")
+
             fname = str(msg_id_int) + ".txt"
             fpath = outdir + '/' + fname
             
             if not headers: string = payload
-
+            
+            if binary: string = string.hex()
+            
             if msg_id_int in ids_written:
                 with open(fpath, 'a') as ofile:
-                    ofile.write(string.hex() + '\n')
+                    ofile.write(string + '\n')
             else:
-                # Start writing fresh.
+                # Start writing fiel from scratch.
                 with open(fpath, 'w') as ofile:
-                    ofile.write(string.hex() + '\n')
+                    ofile.write(string + '\n')
 
                 ids_written.add(msg_id_int)
 
