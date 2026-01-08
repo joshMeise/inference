1df28b638db57d4d1bd5be98e459fc908b7122ec
Josh Meise <joshua.m.meise.24@dartmouth.edu>
2026-01-07 20:20:36 -0500

    message order randomization and plotting

--- baseline/execs/read_file.cpp
+++ baseline/execs/read_file.cpp
@@ -13,6 +13,9 @@
 #include <fstream>
 #include <vector>
 #include <cstdint>
+#include <cstdio>
+#include <algorithm>
+#include <random>
 
 int main(void) {
     Grammar grammar;
@@ -23,7 +26,11 @@ int main(void) {
     std::uintmax_t fs;
     std::vector<MAVLinkMessage> messages;
     MAVLinkMessage message;
-    
+    FILE* gp;
+    int i, bytes_consumed;
+    std::random_device rd;
+    std::mt19937 rng;
+
     fname = "../files/raw_dataset.bin";
 
     // Get number of bytes in file.
@@ -40,22 +47,40 @@ int main(void) {
     ifile.read(reinterpret_cast<char*>(bytes.data()), fs);
 
     // Convert bytes into messages.
-    while (bytes.size() > 0) {
+    bytes_consumed = 0;
+    while (bytes_consumed < bytes.size()) {
         // Create message object.
-        message = MAVLinkMessage(bytes);
+        message = MAVLinkMessage(std::vector<uint8_t>(bytes.begin() + bytes_consumed, bytes.end()));
 
         // Remove bytes from byte vector after conversion to message.
-        bytes.erase(bytes.begin(), bytes.begin() + message.get_len());
+        bytes_consumed += message.get_len();
 
         messages.push_back(message);
     }
 
-    // Make grammar.
+    // Randomize order of messages in vector prior to adding to grammar.
+    rng = std::mt19937(rd());
+    std::shuffle(messages.begin(), messages.end(), rng);
+
+    // Begin plotting process.
+    gp = popen("gnuplot", "w");
+    fprintf(gp, "set title 'Number of terminals and non-terminals in grammar vs. number of messages'\n");
+    fprintf(gp, "set xlabel 'Number of messages'\n");
+    fprintf(gp, "set ylabel 'Number of terminals and non-terminals'\n");
+    fprintf(gp, "unset key\n");
+    fprintf(gp, "set terminal png\n");
+    fprintf(gp, "set output 'plot.png'\n");
+    fprintf(gp, "plot '-' with lines\n");
+
+    // Make grammar and plot.
+    i = 0;
     for (const auto& message : messages) {
         grammar.add_message(message.serialize());
-        std::cout << "Number of terminals/non-terminals = " << grammar.get_num_terminals() + grammar.get_num_non_terminals() << std::endl;
+        fprintf(gp, "%d %d\n", i, grammar.get_num_non_terminals() + grammar.get_num_terminals());
+        i++;
     }
-        
+    
+    pclose(gp);
     ifile.close();
 
     return 0;
